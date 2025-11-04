f702b8b1d6fab38c2e22f064b7103c5869c2e474
pporschen <58578026+pporschen@users.noreply.github.com>
2025-11-04 16:43:34 +0100

    Add validation annotations to Prescription model
    Added validation annotations for Prescription model.

--- app/src/main/java/com/project/back_end/models/Prescription.java
+++ app/src/main/java/com/project/back_end/models/Prescription.java
@@ -1,5 +1,11 @@
 package com.project.back_end.models;
 
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.annotation.Id;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 @Document(collection = "prescriptions")
 public class Prescription {
 
